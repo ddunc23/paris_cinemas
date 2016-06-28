6f8bae123bac1fd2f21af15c66cc31ad3e40590a
Duncan <hay.duncan@gmail.com>
2016-06-28 11:16:03 +0100

    modified template

--- paris_cinemas/settings/secret_settings_template.py
+++ paris_cinemas/settings/secret_settings_template.py
@@ -1,6 +1,6 @@
 # Copy your secret key here and rename the file 'secret_settings.py'
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'elk3z*@2z6e((q=5framwz!pypd!s8@$ih&!8dj=(^!u2^gzm+'
+SECRET_KEY = ''
 
 
 # Database
@@ -9,9 +9,9 @@ SECRET_KEY = 'elk3z*@2z6e((q=5framwz!pypd!s8@$ih&!8dj=(^!u2^gzm+'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'paris_cinemas',
-        'USER': 'paris_cinemas',
-        'PASSWORD': 'cinemas',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
